1c2fce6407ae1f6c7204a9d2e8c457cbd648821a
유수화 <dbtnghk528@gmail.com>
2023-11-28 22:42:31 +0900

    test: fix mock server uri

--- tests/test_healthz.py
+++ tests/test_healthz.py
@@ -4,12 +4,14 @@ import requests_mock
 
 
 def get_health_status():
-    response = requests.get("http://127.0.0.1/healthz")
+    response = requests.get("http://virtumall.com/healthz")
     return response.json()
 
 
 def test_healthz_endpoint():
     with requests_mock.Mocker() as mock:
-        mock.get("http://127.0.0.1/healthz", json={"message": "ok"}, status_code=200)
+        mock.get(
+            "http://virtumall.com/healthz", json={"message": "ok"}, status_code=200
+        )
         response = get_health_status()
         assert response["message"] == "ok"
