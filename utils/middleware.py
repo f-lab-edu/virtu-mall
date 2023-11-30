1e91186b7f4d813fd88ffa35a3a4692360af2178
유수화 <dbtnghk528@gmail.com>
2023-11-30 23:35:15 +0900

    fix: add type annotation to HealthCheckMiddleware

--- utils/middleware.py
+++ utils/middleware.py
@@ -1,11 +1,13 @@
-from django.http import JsonResponse
+from typing import Callable
+
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
 
 class HealthCheckMiddleware:
-    def __init__(self, get_response):
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         if request.path == "/healthz":
             data = {"message": "ok"}
             return JsonResponse(data)
