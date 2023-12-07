01b3182abc52a31058055a6dfc917ee1949b0e38
유수화 <dbtnghk528@gmail.com>
2023-12-07 16:11:46 +0900

    feat: setting installed app and AUTH_USER_MODEL

--- virtumall/settings.py
+++ virtumall/settings.py
@@ -59,6 +59,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "utils",
+    "apps.user",
 ]
 
 MIDDLEWARE = [
@@ -92,6 +95,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "virtumall.wsgi.application"
 
+AUTH_USER_MODEL = "user.User"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
