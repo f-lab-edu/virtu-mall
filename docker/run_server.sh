67a0c78a9103bcc1f104847c07072e31b1b9aaab
유수화 <dbtnghk528@gmail.com>
2023-12-31 17:19:14 +0900

    fix: remove apt cache and setting workers

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PORT=${PORT:-8000}
+WORDERS=${WORDERS:-2}
 python3 manage.py migrate
-gunicorn --bind 0.0.0.0:$PORT --workers=2 virtumall.wsgi:application
+gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS virtumall.wsgi:application
