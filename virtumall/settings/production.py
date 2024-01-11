899f9147dbc0b584199db3b969c5d2cfcc29b66c
유수화 <dbtnghk528@gmail.com>
2024-01-12 00:51:08 +0900

    fix: allowed_hosts to *

--- virtumall/settings/production.py
+++ virtumall/settings/production.py
@@ -2,10 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "http://ec2-15-164-50-94.ap-northeast-2.compute.amazonaws.com",
-    "virtumall_api",
-]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
