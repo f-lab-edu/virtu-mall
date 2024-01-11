a4de4120ef48f7ff7d45943a9b448b106eff7dce
유수화 <dbtnghk528@gmail.com>
2024-01-11 22:59:30 +0900

    fix: allow all host for test

--- virtumall/settings/production.py
+++ virtumall/settings/production.py
@@ -2,10 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "http://ec2-15-164-50-94.ap-northeast-2.compute.amazonaws.com",
-    "nginx",
-]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
