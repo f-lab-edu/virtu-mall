71aa7f413f7fe042eec861f38a198c843c7e64f8
유수화 <dbtnghk528@gmail.com>
2024-01-12 00:19:29 +0900

    fix: nginx settings

--- docker/run_server_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PORT=${PORT:-8000}
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver settings --virtumall.settings.development
--- virtumall/settings/production.py
+++ virtumall/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["http://ec2-15-164-50-94.ap-northeast-2.compute.amazonaws.com"]
 
 DATABASES = {
     "default": {
