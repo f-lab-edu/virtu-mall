8a9b2913d10670a9e5963d59ad4ecc5a27502d35
유수화 <dbtnghk528@gmail.com>
2024-01-31 18:37:51 +0900

    feat: add static urls

--- virtumall/urls.py
+++ virtumall/urls.py
@@ -31,3 +31,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIAFILES_LOCATION
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATICFILES_LOCATION
+    )
