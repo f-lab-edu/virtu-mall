b0715cc4eedc2b73ea5cfb6a9107ebc9cca57c08
유수화 <dbtnghk528@gmail.com>
2024-01-31 17:15:10 +0900

    fix: collect static option

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -3,5 +3,5 @@ PORT=${PORT:-8000}
 WORKERS=${WORKERS:-2}
 python3 manage.py makemigrations
 python3 manage.py migrate
-python manage.py collectstatic
+python3 manage.py collectstatic -i
 gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS virtumall.wsgi:application
