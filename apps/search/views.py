9784ba1343df44c31bf18109242328f537273097
유수화 <dbtnghk528@gmail.com>
2024-02-01 13:33:23 +0900

    feat: search logic

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from apps.product.models import Product
+from apps.product.serializers import ProductSerializer
+
+
+class SearchListAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        query = self.request.query_params.get("q", "")
+        if query:
+            return Product.objects.filter(name__icontains=query)
+        else:
+            return Product.objects.none()
