89a89d6fbab17188081ef199de270d70ffbfa068
유수화 <72597365+aohus@users.noreply.github.com>
2024-03-20 16:43:24 +0900

    Feature/create search api (#43)
    * feat: init search api
    
    * fix: development env
    
    * feat: search logic
    
    * feat: persistent connection option
    
    * fix: conn max age
    
    * fix: created to created_at and fix logger
    
    * feat: create multiple gunicorn app
    
    * fix: host port
    
    * fix: container name
    
    * fix: nginx port
    
    * feat: add django-silk for test
    
    * fix: directory 구조 변경
    
    * fix: docker-compose version
    
    * feat: add pagenation
    
    * fix: remove sonarcloud and add swarm.yml

--- make_venv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3.11 -m venv $1
-source $1/bin/activate
-pip install --upgrade pip
-pip install -I -U -r ./requirements/requirements.txt
--- src/virtumall/settings/base.py
+++ src/virtumall/settings/base.py
@@ -54,21 +54,31 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 # Application definition
 
-INSTALLED_APPS = [
+BUILTIN_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
     "rest_framework",
     "storages",
+    "silk",
+]
+
+LOCAL_APPS = [
     "utils",
     "apps.user",
     "apps.product",
     "apps.cart",
     "apps.payment",
+    "apps.search",
 ]
+
+INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -79,6 +89,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "silk.middleware.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "virtumall.urls"
@@ -109,13 +120,8 @@ AUTH_USER_MODEL = "user.User"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "HOST": "mysql-db",
-        "NAME": "virtumall",
-        "USER": "root",
-        "PASSWORD": "virtumall",
-        "PORT": "3306",
-        "OPTIONS": {"charset": "utf8mb4"},
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -138,6 +144,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -168,25 +178,59 @@ if DEBUG:
     # Default primary key field type
     # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "handlers": {
-            "console": {
-                "level": "DEBUG",
-                "class": "logging.StreamHandler",
-            },
-            "file": {
-                "level": "DEBUG",
-                "class": "logging.FileHandler",
-                "filename": "django_auth_debug.log",
-            },
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {
+        "level": "WARNING",
+    },
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s [%(levelname)-5s %(name)s:%(lineno)s %(funcName)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
         },
-        "loggers": {
-            "django.contrib.auth": {
-                "handlers": ["console", "file"],
-                "level": "DEBUG",
-                "propagate": True,
-            },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
+    },
+    "loggers": {
+        # libraries
+        "django": {
+            "handlers": ["null"],
+            "propagate": True,
+            "level": "WARNING",
+        },
+        "django.db.backends": {
+            "level": "ERROR",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "django_command": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+LOCAL_APP_DIRECTORIES = [app.split(".")[0] for app in LOCAL_APPS]
+
+for local_app_directory in LOCAL_APP_DIRECTORIES:
+    LOGGING["loggers"][local_app_directory] = {
+        "handlers": ["console"],
+        "level": "DEBUG",
+        "propagate": True,
     }
