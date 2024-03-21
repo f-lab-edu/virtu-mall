0f79b151964ded45cdb703f0c7924be2d64ab1a0
aohus <dbtnghk528@gmail.com>
2024-03-21 14:46:00 +0900

    feat: modify gunicorn worker to gevent

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -4,4 +4,5 @@ WORKERS=${WORKERS:-2}
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS virtumall.wsgi:application
+# gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS virtumall.wsgi:application
+gunicorn --bind 0.0.0.0:$PORT --worker-class=gevent --worker-connections=1000 --workers=$WORKERS virtumall.wsgi:application
