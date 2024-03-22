ee86103c6978fc978bdb3815c9fe96d57eb716e0
aohus <dbtnghk528@gmail.com>
2024-03-22 13:22:08 +0900

    fix: gunicorn and nginx conf

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -4,5 +4,6 @@ WORKERS=${WORKERS:-2}
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+gunicorn --bind 0.0.0.0:$PORT --worker-class=gevent --statsd-host=localhost:8125 --worker-connections=1000 --workers=$WORKERS --name=virtumall virtumall.wsgi:application
+# gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS --threads=3 virtumall.wsgi:application
 # gunicorn --bind 0.0.0.0:$PORT --workers=$WORKERS virtumall.wsgi:application
-gunicorn --bind 0.0.0.0:$PORT --worker-class=gevent --worker-connections=1000 --workers=$WORKERS virtumall.wsgi:application
