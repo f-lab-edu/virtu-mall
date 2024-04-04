bbcf306c10dc591c8ed620a056e4cf84199880bb
aohus <dbtnghk528@gmail.com>
2024-04-04 10:04:05 +0900

    fix: db connection max age

--- src/virtumall/settings/production.py
+++ src/virtumall/settings/production.py
@@ -12,7 +12,7 @@ DATABASES = {
         "NAME": get_env_variable("DB_NAME"),
         "USER": get_env_variable("DB_USER"),
         "PASSWORD": get_env_variable("DB_PASSWORD"),
-        "CONN_MAX_AGE": 60,
+        "CONN_MAX_AGE": 30,
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
@@ -20,7 +20,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://ec2-15-164-50-94.ap-northeast-2.compute.amazonaws.com",
+    "http://http://ec2-3-38-119-238.ap-northeast-2.compute.amazonaws.com/",
 ]
 
 AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
