3d3f7ca98395c009544e1ae0a00fd35695ffb198
aohus <dbtnghk528@gmail.com>
2024-08-17 08:58:19 +0900

    fix: wallet model

--- src/apps/payment/models/wallet.py
+++ src/apps/payment/models/wallet.py
@@ -1,22 +1,10 @@
 from django.db import models
-from django.db.models import Sum
-from django.db.models.functions import Coalesce
 
-from apps.payment.models.order import Order
 from apps.user.models import User
 
 
 class Wallet(models.Model):
-    class TransactionType(models.IntegerChoices):
-        DEPOSIT = 0
-        WITHDRAWAL = 1
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transaction_type = models.IntegerField(
-        choices=TransactionType.choices,
-        default=TransactionType.DEPOSIT,
-        null=False,
-    )
     amount = models.PositiveIntegerField(
         verbose_name="amount",
         default=0,
@@ -26,12 +14,6 @@ class Wallet(models.Model):
         verbose_name="mileage",
         default=0,
         null=False,
-    )
-    order = models.ForeignKey(
-        Order,
-        on_delete=models.CASCADE,
-        null=True,
-        related_name="order_wallet",
     )
     deleted_at = models.DateTimeField(
         verbose_name="deleted at",
@@ -47,15 +29,4 @@ class Wallet(models.Model):
 
     @classmethod
     def get_balance(cls, user: User) -> int:
-        deposits = cls.objects.filter(
-            user=user,
-            transaction_type=Wallet.TransactionType.DEPOSIT,
-            deleted_at=None,
-        ).aggregate(amount=Coalesce(Sum("amount"), 0))["amount"]
-
-        withdrawals = cls.objects.filter(
-            user=user,
-            transaction_type=Wallet.TransactionType.WITHDRAWAL,
-            deleted_at=None,
-        ).aggregate(amount=Coalesce(Sum("amount"), 0))["amount"]
-        return deposits - withdrawals
+        return cls.objects.get(user=user, deleted_at=None).amount
